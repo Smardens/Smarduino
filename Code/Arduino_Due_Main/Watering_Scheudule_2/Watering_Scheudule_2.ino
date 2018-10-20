ad2267173e6a737fb94e7dcec341df70cfa7b444
Caitlin Rubia <caitlinrubia@gmail.com>
2018-10-19 22:36:49 -0700

    working water scheudule that writes the folder and files to store readings. it also reads the readings sent by the esp

--- Code/Arduino_Due_Main/Watering_Scheudule_2/Watering_Scheudule_2.ino
+++ Code/Arduino_Due_Main/Watering_Scheudule_2/Watering_Scheudule_2.ino
@@ -1,61 +1,62 @@
-
 /**
-* SMARDENS- THE RESILIENT SMART GARDEN
-* SMARDUINO 2.0.0
-*
-* Arduino: DUE
-* Summary: Watering Schedule and Hardware Valve/Sensor Test
-* The Arduino will obtain data from the Wifi Module ESP 8266 Huzzah through serial communication.
-* It will read and store the 5 soil moisture sensor, 2 UV sensor, and 2 Humidity/Temperature sensor readings every three
-* hours; therefore it will read and store 8 data readings per sensor per day [3am, 6am, 9am, 12pm, 3pm, 6pm, 9pm, 12am].
-* Based on the algorithm to determine whether to water, the Arduino is programmed to water at time1 for a regular
-* water, and if the soil is still dry it will water again at time2.
-*
-* Notes:
-* To customize the schedule, change the following variables:
-*  -DATE: to the current date when the Arduino is powered
-*  -TIME: to the current time when the Arduino is powered
-*  -REG_WATERING_DURATION: to the number of minutes the garden is watered if the soil is dry
-*  -SHORT_WATERING_DURATION: to the number of minutes the garden is watered if the soil is moist
-*
-* To customize the pin inputs, change the following variables:
-*  -SOLENOIDPIN: to the digital pin on the Arduino
-*  -DHT1PIN: to the digital pin on the Arduino
-*  -SOILMOISTUREPIN1: to the analog pin on the Arduino
-*  -SOILMOISTUREPIN2: to the analog pin on the Arduino
-*  -UV1PIN: to the analog pin on the Arduino
-*
-* Date: 10/16/18
-*
-* Created by:
-* Caitlin Rubia
-* Xinyi Chen
-* Brian Powell
-* Israel Grogin
-*
-* Reference:
-* https://smardens.com/
-*
-* Resources:
-* Clock: https://playground.arduino.cc/code/time
-* SD Card Module: https://www.arduino.cc/en/Reference/SD
-* SPI: https://www.arduino.cc/en/Reference/SPI
-*
-* Modified Date:
-*
-* Modified by:
-*
+SMARDENS- THE RESILIENT SMART GARDEN
+SMARDUINO 2.1.0
+
+Arduino: DUE
+Summary: Watering Schedule and Hardware Valve/Sensor Test
+The Arduino will obtain data from the Wifi Module ESP 8266 Huzzah through serial communication.
+It will read and store the 5 soil moisture sensor, 2 UV sensor, and 2 Humidity/Temperature sensor
+readings every three hours; therefore it will read and store 8 data readings per sensor per day.
+Based on the algorithm to determine whether to water, the Arduino is programmed to water at time1
+for a regular water, and if the soil is still dry it will water again at time2.
+
+Notes:
+To customize the schedule, change the following variables:
+-REG_WATERING_DURATION: to the number of minutes the garden is watered if the soil is dry
+-SHORT_WATERING_DURATION: to the number of minutes the garden is watered if the soil is moist
+-WATERING_TIME_1: to the hour of the day for the regular watering time (ideally morning, ie: 8AM, enter 8)
+-WATERING_TIME_2: to the hour of the day for the extra watering time (ideally afternoon; ie: 5PM, enter 17)
+-CURRENT_MONTH: setting the month
+-CURRENT_DAY: setting the day
+-CURRENT_YEAR: setting the year
+-YEAR_ABBR: second part of the current year (ie: year 2018, enter 18)
+-CURRENT_HOUR: setting the hour
+-CURRENT_MINUTE: setting the minutes
+AIRVALUE:
+WATERVALUE: 
+
+To customize the pin inputs, change the following variables:
+-SOLENOIDPIN: to the digital pin on the Arduino
+-SDPIN: to digital pin on arduino
+
+Date: 10/19/18
+
+Created by:
+Caitlin Rubia
+Xinyi Chen
+Brian Powell
+Israel Grogin
+
+Reference:
+https://smardens.com/
+
+Resources:
+Clock: https://playground.arduino.cc/code/time
+SD Card Module: https://www.arduino.cc/en/Reference/SD
+SPI: https://www.arduino.cc/en/Reference/SPI
+
+Modified Date:
+
+Modified by:
+
 */
 #include <TimeLib.h>
 #include <SD.h>
 #include <SPI.h>
 
 //Define Pin Locations
-#define RX 0 //digital for serial communication
-#define TX 1 //digital for serial communication 
 #define SOLENOIDPIN 11 //digital
-#define SDPIN 52 //digital
-
+#define SDPIN 9 //digital
 
 //Custom Values
 int REG_WATERING_DURATION = 10; //10- minuntes adjusted for slow drip for a regular watering duration
@@ -65,79 +66,47 @@ int WATERING_TIME_1 = 8; //8AM - hour of the day in the morning (regular waterin
 int WATERING_TIME_2 = 18; //6PM- hour of the day in the afternoon (extra watering time)
 
 int CURRENT_MONTH = 10;//current month
-int CURRENT_DAY = 16;//current day
+int CURRENT_DAY = 18;//current day
 int CURRENT_YEAR = 2018;//current year
+int YEAR_ABBR = 18; //second part of the current year (ie: year 2018, enter 18)
 
 int CURRENT_HOUR = 7; //current hour of the day
 int CURRENT_MINUTE = 40;//current minute of the day
 
-//Durations
+//Determinant Values for Soil Moisture Dryness Intervals
+const int AIRVALUE = 785;
+const int WATERVALUE = 391;
+
+//Durations for Watering
 unsigned long duration1 = 1000UL * 60 * REG_WATERING_DURATION;
 unsigned long duration2 = 1000UL * 60 * SHORT_WATERING_DURATION;
 
-//Sensor Readings
-String sm1;
-String sm2;
-String sm3;
-String sm4;
-String sm5;
-String uv1;
-String uv2;
-String ht1;
-String ht2;
+//Soil Moisture value determinants for dryness
+int soil1, soil2, soil3, soil4, soil5;
 
-//Soil Moisture Values
-int soil1;
-int soil2;
-int soil3;
-int soil4;
-int soil5;
-
-//Date
-String date;
-int dayC;
-int monthC;
-int yearC;
-
-//Time
-String timeCapture;
-int hourC;
-int minuteC;
-int secondC;
-
-//Data and Files
-String data;
-File dataFile;
-
-//File Name Directory
-String homeDir = "Smardens_Garden_Data";
-
-//boolean to establish valid file pathways
-bool smardensGarden = false;
+//Foler and File Creation for Sensor Readings
+String data; //the sensor date taht will be stored
+File dataFile; //txt. file being created
+const char *rootPath = "Smardens"; //root folder name- Smardens
+char dirName[100]; //filepath size allocated 
+String path = ""; //filepath name
+bool smardensGarden = false; //boolean to establish valid file pathways
 
 void setup()
 {
-  Serial.begin(300);
-  Serial.print("COME ON");
+  /*Setting the current time*/
+  setTime(CURRENT_HOUR, CURRENT_MINUTE, 00, CURRENT_DAY, CURRENT_MONTH, CURRENT_YEAR); // example: 16:00:00 18-10-2018
+
+  /*Serial setup*/
+  Serial.begin(115200); //Arduino Due Serial
+  Serial1.begin(115200); //WIFI Module ESP 8266 Huzzah Serial for TX1, RX1
 
   /*Pin setup*/
   pinMode(SOLENOIDPIN, OUTPUT); //set solenoid pin to control valve
-  pinMode(RX, INPUT); //set receiving pin as the input
-  pinMode(TX, OUTPUT); //set the transmiting pin as the output
 
   /*SD and Directory setup*/
   SDSetup();
-
-  setTime(CURRENT_HOUR, CURRENT_MINUTE, 00, CURRENT_MONTH, CURRENT_DAY, CURRENT_YEAR); // example: 16:00:00 10-10-2018 
-}
-
-/*Watering Schedule*/
-void impl()
-{
-  waterSet();
-  if (hour() % 3 == 0) {
-    createFile();
-  }
+  buildRoot();
 }
 
 /*Initializing SD card*/
@@ -153,172 +122,123 @@ void SDSetup()
     // don't do anything more:
     while (1);
   }
-  Serial.println("card initialized.");
-  buildDirectory();
+  Serial.println("Card Initialized.");
 }
 
-/*Checks if pathway exists*/
-bool buildDir(String pathway)
+/*Checks if filepath is valid to be created*/
+bool buildValid(const char *dir)
 {
-  if (!SD.exists(pathway)) {
-    smardensGarden = SD.mkdir(pathway);
+  smardensGarden = false;
+  if (!SD.exists(dir)) {
+    smardensGarden = SD.mkdir(dir);
   }
   return smardensGarden;
 }
 
-/*Getting the current date*/
-String getSetDate()
+/*Create the root folder*/
+void buildRoot()
 {
-  // get date to check folder directory
-  dayC = day();
-  monthC = month();
-  yearC = year();
-  // convert date variables to string date
-  date = String(dayC) + "_" + String(monthC) + "_" + String(yearC);
-  return date;
-}
-
-/*Create Home Folder*/
-void buildDirectory()
-{
-  // intializing home directory
-  if (buildDir(homeDir)) {
-    Serial.println("Creating new Smarden Folder");
+  if (buildValid(rootPath)) {
+    Serial.println("Creating Smardens Folder");
   }
   else {
-    Serial.println("Error creating new Smarden Folder");
+    Serial.println("Error- Can't Creating Smardens Folder");
   }
 }
 
-/*Getting the current time*/
-String getSetTime()
+/*Creates the date/time folder containing the files*/
+void buildSubRoot()
 {
-  // get time to check folder directory
-  hourC = hour();
-  minuteC = minute();
-  secondC = second();
-  // convert date variables to string date
-  timeCapture = String(hourC) + "_" + String(minuteC) + "_" + String(secondC);
-  return timeCapture;
-}
-
-/*Creates the date folder containing the files*/
-void buildDateFolder()
-{
-  date = getSetDate();
-  // create sub-directory based on date if it doesn't exist
-  if (buildDir(homeDir + "/" + date)) {
-    Serial.println("Creating new Date Folder");
-  }
-  else { //add to the existing sub-directory
-    Serial.println("Date Folder alread exists");
-  }
-  timeCapture = getSetTime();
-
-  // create sub-directory based on time
-  // time should be Military time
-  if (buildDir(homeDir + "/" + date + "/" + timeCapture)) {
-    Serial.println("Creating new Smarden Folder");
+  sprintf(dirName, "%s/%d_%d_%d/%d_%d_%d", rootPath, month(), day(), YEAR_ABBR, hour(), minute(), second());
+  if (buildValid(dirName)) {
+    Serial.println("Creating Date/Time Folder");
+    path = String(dirName); //the filepath that contains the mm_dd_yy folder and the hh_mm_ss folder in it
   }
   else {
-    Serial.println("Error creating new Smarden Folder");
+    Serial.println("Error- Can't Creating Date/Time Folder");
   }
-
 }
 
-/*Creates the file where the data is written*/
-void createFile()
-{
-  buildDateFolder();
-
-  Serial.println("Arduino is listening");
-
-  data = Serial.readString();
-  String idType = data.substring(11, 12); //"ID"
-  String idNum = data.substring(13, 14); //"##"
-
-  sensorReading(idType, idNum);
-
-}
-
-/*Senosor readings to be stored in a file and store the previous readings */
+/*Senosor readings to be stored in a file */
 void sensorReading(String idType, String idNum)
 {
   if (idType.equals("SM")) {
-    File dataFile = SD.open(date + "/" + timeCapture + "/" + "SM.txt", FILE_WRITE);
+    dataFile = SD.open(path + "/SM.txt", FILE_WRITE);
     if (idNum.equals("01")) {
-      sm1 = Serial.println(data);
+      Serial.println("SM01 written");
       soil1 = data.substring(30).toInt();
     }
-    if (idNum.equals("02")) {
-      sm2 = Serial.println(data);
+    else if (idNum.equals("02")) {
+      Serial.println("SM02 written");
       soil2 = data.substring(30).toInt();
     }
-    if (idNum.equals("03")) {
-      sm3 = Serial.println(data);
+    else if (idNum.equals("03")) {
+      Serial.println("SM03 written");
       soil3 = data.substring(30).toInt();
     }
-    if (idNum.equals("04")) {
-      sm4 = Serial.println(data);
+    else if (idNum.equals("04")) {
+      Serial.println("SM04 written");
       soil4 = data.substring(30).toInt();
     }
-    if (idNum.equals("05")) {
-      sm5 = Serial.println(data);
+    else if (idNum.equals("05")) {
+      Serial.println("SM05 written");
       soil5 = data.substring(30).toInt();
     }
     else {
-      Serial.println("id not found");
+      Serial.println("idType = SM; idNum not found");
     }
   }
-  if (idType.equals("UV")) {
-    File dataFile = SD.open(date + "/" + timeCapture + "/" + "UV.txt", FILE_WRITE);
+  else if (idType.equals("UV")) {
+    dataFile = SD.open(path + "/UV.txt", FILE_WRITE);
     if (idNum.equals("01")) {
-      uv1 = Serial.println(data);
+      Serial.println("UV01 written");
     }
-    if (idNum.equals("02")) {
-      uv2 = Serial.println(data);
+    else if (idNum.equals("02")) {
+      Serial.println("UV02 written");
     }
     else {
-      Serial.println("id not found");
+      Serial.println("idType = UV; idNum not found");
     }
   }
-  if (idType.equals("HT")) {
-    File dataFile = SD.open(date + "/" + timeCapture + "/" + "HT.txt", FILE_WRITE);
+  else if (idType.equals("HT")) {
+    dataFile = SD.open(path + "/HT.txt", FILE_WRITE);
     if (idNum.equals("01")) {
-      ht1 = Serial.println(data);
+      Serial.println("HT01 written");
     }
-    if (idNum.equals("02")) {
-      ht2 = Serial.println(data);
+    else if (idNum.equals("02")) {
+      Serial.println("HT02 written");
     }
     else {
-      Serial.println("id not found");
+      Serial.println("idType = HT; idNum not found");
     }
   }
   else {
-    Serial.println("error opening txt");
+    Serial.println("idType not found");
   }
 
   if (dataFile) {
+    Serial.println("Text file is open and will be written in");
     dataFile.println(data);
     dataFile.close();
+  }
+  else {
+    Serial.println("Text file couldn't be opened");
   }
 }
 
 /*Moisture level check as dry, moist, wet*/
 String checkDry(int dryness)
 {
-  const int AirValue = 785;
-  const int WaterValue = 391;
-  int intervals = (AirValue - WaterValue) / 3;
-  if (dryness > WaterValue && dryness < (WaterValue + intervals))
+  int intervals = (AIRVALUE - WATERVALUE) / 3;
+  if (dryness > WATERVALUE && dryness < (WATERVALUE + intervals))
   {
     return "Wet";
   }
-  else if (dryness >(WaterValue + intervals) && dryness < (AirValue - intervals))
+  else if (dryness >(WATERVALUE + intervals) && dryness < (AIRVALUE - intervals))
   {
     return "Moist";
   }
-  else if (dryness < AirValue && dryness >(AirValue - intervals))
+  else if (dryness < AIRVALUE && dryness >(AIRVALUE - intervals))
   {
     return "Dry";
   }
@@ -354,10 +274,9 @@ void waterSet()
   int dryness;
   String check;
 
-
   dryness = (soil1 + soil2 + soil3 + soil4 + soil5) / 5; //average of 5 soil moisture level
 
-  //5 minute before normal watering schedule, check the soil moisture level to determine length of watering
+                               //5 minute before normal watering schedule, check the soil moisture level to determine length of watering
   if (hour() == WATERING_TIME_2 - 1 && minute() == 55 && second() == 0) {
     check = checkDry(dryness);
     if (check == "Dry") {
@@ -370,8 +289,8 @@ void waterSet()
       currentDuration = 0; //don't open if the field is super wet
     }
   }
-  
-  // second watering check time 
+
+  // second watering check time
   if (hour() == WATERING_TIME_2 - 1 && minute() == 55 && second() == 0) {
     //check last soil moisture level
     dryness = (soil1 + soil2 + soil3 + soil4 + soil5) / 5; //average of 5 soil moisture level
@@ -383,19 +302,37 @@ void waterSet()
       currentDuration = 0; //don't open if the field is super wet
     }
   }
-  
+
   firstWater(currentDuration);
   secondWater(currentDuration);
 }
 
+/*Test Valve*/
 void test() {
   digitalWrite(SOLENOIDPIN, HIGH); //open valve
   delay(1000 * 30); //1000 = 1 second
   digitalWrite(SOLENOIDPIN, LOW); //close valve
 }
 
+/*Sensor Readings from Wifi Module ESP 8266 Huzzah- Access Point*/
+void impl() {
+  waterSet(); //waters at certain time for certain durations
+  while (Serial1.available() > 0) { //reading from WIFI Module ESP 8266 Huzzah acting as Access Point
+    buildSubRoot(); //builds subfolders based on the current date and time
+    Serial.println("Arduino is listening");
+    data = Serial1.readString(); //String data = "Sensor ID: TY##" 
+    String idType = data.substring(11, 12); //"TY"
+    String idNum = data.substring(13, 14); //"##"
+    Serial.println(">>>>>>>>>>>>>>Start>>>>>>>>>>>>> \n" + data + "\n>>>>>>>>>>>>>>END>>>>>>>>>>>>>");
+    Serial.println("idType: " + idType);
+    Serial.println("idNum: " + idNum);
+    sensorReading(idType, idNum);
+  }
+}
+
 void loop()
 {
   //test();
   impl();
+
 }
